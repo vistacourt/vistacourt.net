1c7af636a1bdecdf363df3e71af2d3337e60312d
BuildTools <unconfigured@null.spigotmc.org>
2016-10-28 19:13:30 -0700

    test

---
+++ js/vistacourt.js
@@ -0,0 +1,32 @@
+
+
+function displayDate(id){
+	// document.getElementById("dateid").innerHTML=Date();
+	    date = new Date;
+        year = date.getFullYear();
+        month = date.getMonth();
+        months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+        d = date.getDate();
+        day = date.getDay();
+        days = new Array('Sunday, ', 'Monday, ', 'Tuesday, ', 'Wednesday, ', 'Thursday, ', 'Friday, ', 'Saturday');
+        h = date.getHours();
+        if(h<10)
+        {
+                h = "0"+h;
+        }
+        m = date.getMinutes();
+        if(m<10)
+        {
+                m = "0"+m;
+        }
+        s = date.getSeconds();
+        if(s<10)
+        {
+                s = "0"+s;
+        }
+        result = ''+days[day]+' '+months[month]+' '+d+' '+year+' '+h+':'+m+':'+s;
+        document.getElementById("dateid").innerHTML = result;
+        setTimeout('displayDate("'+id+'");','1000');
+        return true;
+	
+}
