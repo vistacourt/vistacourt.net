23d47f663cfd06dce9b23381066622db29e7d2de
vistacourt <support@vistacourt.net>
2016-12-08 14:26:44 -0800

    alien

--- js/vistacourt.js
+++ js/vistacourt.js
@@ -28,5 +28,25 @@ function displayDate(id){
         document.getElementById("dateid").innerHTML = result;
         setTimeout('displayDate("'+id+'");','1000');
         return true;
-	
+
+}
+
+function passWord() {
+	var testV = 1;
+	var pass1 = prompt('Please Enter Your Password',' ');
+	while (testV < 3) {
+	if (!pass1)
+	history.go(-1);
+	if (pass1.toLowerCase() == "617661") {
+	// alert('You Got it Right!');
+	window.open('main.html');
+	break;
+	}
+	testV+=1;
+	var pass1 =
+	prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
+	}
+	if (pass1.toLowerCase()!="password" & testV ==3)
+	history.go(-1);
+	return " ";
 }
