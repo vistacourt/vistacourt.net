3560f3a636b5917e17ed4240fd7003f29515ce49
Thomas Kelly <Thomas Kelly>
2017-03-15 18:53:38 -0700

    MSI - disabled crappy js password

--- js/vistacourt.js
+++ js/vistacourt.js
@@ -32,21 +32,21 @@ function displayDate(id){
 }
 
 function passWord() {
-	var testV = 1;
-	var pass1 = prompt('Please Enter Your Password',' ');
-	while (testV < 3) {
-	if (!pass1)
-	history.go(-1);
-	if (pass1.toLowerCase() == "617661") {
-	// alert('You Got it Right!');
+	// var testV = 1;
+	// var pass1 = prompt('Please Enter Your Password',' ');
+	// while (testV < 3) {
+	// if (!pass1)
+	// history.go(-1);
+	// if (pass1.toLowerCase() == "617661") {
+	// // alert('You Got it Right!');
 	window.open('main.html');
-	break;
-	}
-	testV+=1;
-	var pass1 =
-	prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
-	}
-	if (pass1.toLowerCase()!="password" & testV ==3)
-	history.go(-1);
-	return " ";
+	// break;
+	// }
+	// testV+=1;
+	// var pass1 =
+	// prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
+	// }
+	// if (pass1.toLowerCase()!="password" & testV ==3)
+	// history.go(-1);
+	// return " ";
 }
