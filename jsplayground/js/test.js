e30a3a2207ea1d551b0fdc03c70437fa98c8e26c
Thomas Kelly <Thomas Kelly>
2017-03-21 17:46:13 -0700

    new folder

---
+++ jsplayground/js/test.js
@@ -0,0 +1,15 @@
+
+
+
+function calculate(){
+  r1 = Math.floor((Math.random() * 6) + 1);
+  r2 = Math.floor((Math.random() * 6) + 1);
+  r3 = Math.floor((Math.random() * 6) + 1);
+  r4 = Math.floor((Math.random() * 6) + 1);
+  r5 = Math.floor((Math.random() * 6) +1)
+  document.getElementById("c1").innerHTML = r1;
+  document.getElementById("c2").innerHTML = r2;
+  document.getElementById("c3").innerHTML = r3;
+  document.getElementById("c4").innerHTML = r4;
+  document.getElementById("c5").innerHTML = r5;
+}
