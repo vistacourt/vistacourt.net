334e3d1e4689beaab8152c8f4689b09ab5269bca
Thomas Kelly <vistacourt.support@gmail.com>
2017-10-01 14:48:43 -0700

    msi - weather

--- js/weather.js
+++ js/weather.js
@@ -6,28 +6,19 @@ document.getElementById("button2").onclick = function() {getType()};
 });
 
 var t = 'f';
-
 function getType(){
-  if (t === 'f'){
-    var t = 'c';
+  if (t == 'f'){
+     t = 'c';
   }
   else {
-    var t = 'f';
+     t = 'f';
     }
+getZip();
   }
-
-
-
 
 function getZip() {
     document.getElementById("button").innerHTML = "Submit";
 $(document).ready;
-    console.log(document.getElementById('zip').value);
-
-
-
-
-
     $.simpleWeather({
       location: document.getElementById('zip').value,
       woeid: '',
@@ -38,27 +29,21 @@ $(document).ready;
         html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
         html += '<li class="currently">'+weather.currently+'</li>';
         html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
-          console.log(weather.currently);
-
             if (weather.currently.includes('Cloudy')){
                 document.body.style.backgroundImage = "url('http://cdn.weatheravenue.com/img/background/background.jpg')";
               }
-
             if (weather.currently.includes('Sunny') || weather.currently.includes('Clear')) {
                 console.log('sunny');
                 document.body.style.backgroundImage = "url('http://runitlikeamom.com/wp-content/uploads/2016/03/Sunshine-and-Rainbows.jpg')";
               }
-
             if (weather.currently.includes('Rain') || weather.currently.includes('Showers') || weather.currently.includes('Thunderstorms')){
                 console.log('rain');
                 document.body.style.backgroundImage = "url('https://wallpapercave.com/wp/oScSlk7.jpg')";
             }
-
             if (weather.currently.includes('Snow') || weather.currently.includes('Showers') || weather.currently.includes('Thunderstorms')){
                 console.log('rain');
                 document.body.style.backgroundImage = "url('http://findingfabulous.co.za/wp-content/uploads/2016/06/DSC_0408.jpg')";
               }
-
             else{
               console.log('fail');
             }
@@ -69,6 +54,4 @@ $(document).ready;
         $("#weather").html('<p>'+error+'</p>');
       }
     });
-
-
 }
