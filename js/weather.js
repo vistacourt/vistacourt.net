75d5f2660d201f67405361a5124237f6c5f0d7bb
Thomas Kelly <vistacourt.support@gmail.com>
2017-10-01 15:06:32 -0700

    msi - weather

--- js/weather.js
+++ js/weather.js
@@ -33,15 +33,12 @@ $(document).ready;
                 document.body.style.backgroundImage = "url('http://cdn.weatheravenue.com/img/background/background.jpg')";
               }
             if (weather.currently.includes('Sunny') || weather.currently.includes('Clear')) {
-                console.log('sunny');
                 document.body.style.backgroundImage = "url('http://runitlikeamom.com/wp-content/uploads/2016/03/Sunshine-and-Rainbows.jpg')";
               }
             if (weather.currently.includes('Rain') || weather.currently.includes('Showers') || weather.currently.includes('Thunderstorms')){
-                console.log('rain');
                 document.body.style.backgroundImage = "url('https://wallpapercave.com/wp/oScSlk7.jpg')";
             }
             if (weather.currently.includes('Snow') || weather.currently.includes('Showers') || weather.currently.includes('Thunderstorms')){
-                console.log('rain');
                 document.body.style.backgroundImage = "url('http://findingfabulous.co.za/wp-content/uploads/2016/06/DSC_0408.jpg')";
               }
             else{
