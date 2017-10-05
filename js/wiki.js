6fbba012dfe111d9db2a3011b34799f974cdaa8b
Kelly <tk8372@CB.COM>
2017-10-05 15:00:57 -0700

    lenovo - struggling

--- js/wiki.js
+++ js/wiki.js
@@ -1,18 +1,39 @@
+
+
+
 function getSearch(){
+var searchInput = document.getElementById('search');
 
 
-var a = "test123";
 
+  $(document).ready(function(){
 
-  document.querySelector("button").addEventListener("click", function(){
-      document.querySelector('div').style.display = "block";
+      $.ajax({
+          type: "GET",
+          url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=" && searchInput.value,
+          contentType: "application/json; charset=utf-8",
+          async: false,
+          dataType: "json",
+          success: function (data, textStatus, jqXHR) {
+              console.log(data);
+          },
+          error: function (errorMessage) {
+          }
+      });
   });
 
 
 
 
+console.log(searchInput.value);
+
 
 }
+
+
+
+
+
 
 function getRandom(){
 window.open('https://en.wikipedia.org/wiki/Special:Random')
