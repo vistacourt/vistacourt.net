9acdadd26ca56affac33d0d022ea4451ec72b35a
Thomas Kelly <vistacourt.support@gmail.com>
2017-10-06 14:08:17 -0700

    msi - index image

--- js/wiki.js
+++ js/wiki.js
@@ -2,32 +2,94 @@
 
 
 function getSearch(){
-var searchInput = document.getElementById('search');
-
-
+var queryStr = document.getElementById('search');
 
   $(document).ready(function(){
 
-      $.ajax({
-          type: "GET",
-          url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=" && searchInput.value,
-          contentType: "application/json; charset=utf-8",
-          async: false,
-          dataType: "json",
-          success: function (data, textStatus, jqXHR) {
-              console.log(data);
-          },
-          error: function (errorMessage) {
-          }
-      });
+/*    $.ajax( {
+        url: remoteUrlWithOrigin,
+        data: queryData,
+        dataType: 'json',
+        type: 'POST',
+        headers: { 'Api-User-Agent': 'Example/1.0' },
+        success: function(data) {
+           // do something with data
+        }
+    } );
+
+$.ajax({
+  url: '//www.wikidata.org/w/api.php',
+  data: { action: 'wbgetentities', ids: mw.config.get('wgWikibaseItemId'), format: 'json' },
+  dataType: 'jsonp',
+  success: function (x) {
+    console.log('wb label', x.entities.Q39246.labels.en.value);
+    console.log('wb description', x.entities.Q39246.descriptions.en.value);
+  }
+});
+
+*/
+//
+// var mw;
+// (function (mw) {
+//
+//   /**
+//    * Query a MediaWiki api.php instance with the given options
+//    */
+//   function mwQuery(endpoint, options) {
+//
+//     /**
+//      * Create a uniquely-named callback that will process the JSONP results
+//      */
+//     var createCallback = function (k) {
+//       var i = 1;
+//       var callbackName;
+//       do {
+//         callbackName = 'callback' + i;
+//         i = i + 1;
+//       } while (window[callbackName])
+//       window[callbackName] = k;
+//       return callbackName;
+//     }
+//
+//     /**
+//      * Flatten an object into a URL query string.
+//      * For example: { foo: 'bar', baz: 42 } becomes 'foo=bar&baz=42'
+//      */
+//     var queryStr = function (options) {
+//       var query = [("hockey")];
+//       for (var i in options) {
+//         if (options.hasOwnProperty(i)) {
+//           query.push(encodeURIComponent(i) + '=' + encodeURIComponent(options[i]));
+//         }
+//       }
+//       return query.join('&');
+//     }
+//
+//     /**
+//      * Build a function that can be applied to a callback.  The callback processes
+//      * the JSON results of the API call.
+//      */
+//     return function (k) {
+//       options.format = 'json';
+//       options.callback = createCallback(k);
+//       var script = document.createElement('script');
+//       script.src = endpoint + '?' + queryStr(options);
+//       var head = document.getElementsByTagName('head')[0];
+//       head.appendChild(script);
+//     };
+//
+//   }
+//
+//   mw.api = {
+//     query: mwQuery,
+//   };
+//
+// })(mw || (mw = {}));
+// console.log(mw.api);
+// console.log(mwQuery);
   });
-
-
-
-
-console.log(searchInput.value);
-
-
+  
+console.log(queryStr.value);
 }
 
 
