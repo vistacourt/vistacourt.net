75ca903c71008e070a41192bd0c4f7b12ab91e1d
Thomas Kelly <vistacourt.support@gmail.com>
2017-10-07 19:09:02 -0700

    msi - index image

---
+++ js/test.js
@@ -0,0 +1,24 @@
+
+
+
+function auto(){
+
+
+
+$("#searchform").autocomplete({
+    source: function(request, response) {
+        $.ajax({
+            url: "http://en.wikipedia.org/w/api.php",
+            dataType: "jsonp",
+            data: {
+                'action': "opensearch",
+                'format': "json",
+                'search': request.term
+            },
+            success: function(data) {
+                response(data[1]);
+            }
+        });
+    }
+});
+}
